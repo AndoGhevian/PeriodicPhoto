c3deef2f5540d8450e34d905e832e14bf4d6259f
AndoGhevian <lenyvonbeer@gmail.com>
2023-01-17 11:25:35 +0400

    FullScreen Handling added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,82 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useImmer } from 'use-immer'
+import fscreen from 'fscreen'
+import Camera from './components/Camera';
 import './App.css';
 
 function App() {
+  const [state, updateState] = useImmer({
+    error: "",
+    isRunning: false,
+  })
+  const appRef = useRef<HTMLDivElement>(null)
+
+  const tryFullscreen = () => {
+    const appDiv = appRef.current
+    if (appDiv) {
+      (fscreen.requestFullscreen(appDiv) as unknown as Promise<any>)
+        .catch((err) => updateState({
+          error: err.message,
+          isRunning: false,
+        }))
+    }
+  }
+
+  const fullscreenerrorHandler = () => { }
+
+  const fullscreenchangeHandler = () => {
+    if (fscreen.fullscreenElement === null) {
+      updateState({
+        error: "",
+        isRunning: false,
+      })
+    } else if (fscreen.fullscreenElement) {
+      updateState({
+        error: "",
+        isRunning: true,
+      })
+    }
+  }
+
+  useEffect(() => {
+    if (!fscreen.fullscreenEnabled) {
+      updateState(draft => {
+        draft.error = "ErrNotSupproted: App Is Require Fullscreen Feature!"
+      })
+    }
+  }, [])
+
+  useEffect(() => {
+    if (fscreen.fullscreenEnabled) {
+      fscreen.addEventListener('fullscreenerror', fullscreenerrorHandler)
+      fscreen.addEventListener('fullscreenchange', fullscreenchangeHandler)
+    }
+    return () => {
+      fscreen.removeEventListener('fullscreenerror', fullscreenchangeHandler)
+      fscreen.removeEventListener('fullscreenchange', fullscreenchangeHandler)
+    }
+  })
+
+  const {
+    error,
+    isRunning,
+  } = state
+
   return (
-    <div className="App">
-      App
+    <div ref={appRef} className="App">
+      {!isRunning ? (
+        <>
+          {error ? (
+            <h3>{error}</h3>
+          ) : (
+            <button
+              onClick={tryFullscreen}
+            >Fullscreen(F11)</button>
+          )}
+        </>
+      ) : (
+        <Camera />
+      )}
     </div>
   );
 }
---
+++ src/components/Camera.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useRef } from 'react';
+import { useImmer } from 'use-immer'
+import './Camera.css';
+
+function Camera() {
+  return (
+    <div className="Camera">
+      Camera
+    </div>
+  );
+}
+
+export default Camera;
